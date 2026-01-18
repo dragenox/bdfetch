2718bfdff561355995d99549f8a8f12684379ae7
Ishan D. Gupta <ishan.d.gupta@outlook.com>
2026-01-18 20:58:20 +0530

    Make remote installer work with wget or curl

--- install-remote.sh
+++ install-remote.sh
@@ -4,8 +4,19 @@ set -e
 BIN_URL="https://raw.githubusercontent.com/dragenox/bdfetch/main/bdfetch"
 INSTALL_PATH="/usr/local/bin/bdfetch"
 
-echo "[+] Downloading bdfetch..."
-curl -fsSL "$BIN_URL" -o "$INSTALL_PATH"
+echo "[+] Installing bdfetch..."
+
+if command -v curl >/dev/null 2>&1; then
+    echo "[+] Using curl"
+    curl -fsSL "$BIN_URL" -o "$INSTALL_PATH"
+elif command -v wget >/dev/null 2>&1; then
+    echo "[+] Using wget"
+    wget -qO "$INSTALL_PATH" "$BIN_URL"
+else
+    echo "[!] Neither curl nor wget found"
+    echo "    Please install one of them and retry."
+    exit 1
+fi
 
 chmod +x "$INSTALL_PATH"
 
